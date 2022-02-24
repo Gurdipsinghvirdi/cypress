4911fe7e96478ba8bb5397b1ba6fa735529f932e
Gurdip <gurdip.singh@seera.sa>
2022-02-24 13:42:29 +0400

    first commit

---
+++ cypress/integration/session/xpec.spec.js
@@ -0,0 +1,12 @@
+describe('Cypress session', function() {
+    it('Cypress Commands', function() {
+    cy.visit("https://qa-frontend-cypress.s3.eu-west-1.amazonaws.com/cypress-session2.html");
+    cy.get('[name=namec]').type('Sylar')
+    cy.get('input[placeholder^=Street]').type('Sylar')
+    cy.get('input[name=zipcode]').type('123')
+    cy.get('select').select('USA')
+    cy.get('input[name=email]').type('gurdip.singh@seera.sa')
+    cy.get('[type="checkbox"]').check(['Prpoposal', 'Abstract'],{ force: true })
+    cy.get('[type="submit"]').click()
+    });
+});
